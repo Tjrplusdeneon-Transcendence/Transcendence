ec8cfe788394d1b6230f8265c158dbd3f26fdc68
Nora <105424123+Noralya@users.noreply.github.com>
2024-08-13 13:18:25 +0200

    refactor: fix typos

--- frontend/game.js
+++ frontend/game.js
@@ -1,3 +1,6 @@
+// /!\ Trop long. A trier dans des fichiers differents
+
+// En vrai c'est pas une "ia" a proprement parlé donc ne compte pas vraiment dans le module ?
 
 const canvas = document.getElementById('pongCanvas');
 const context = canvas.getContext('2d');
@@ -8,18 +11,19 @@ let paddleY2 = (canvas.height - paddleHeight) / 2;
 const ballRadius = 10;
 let x = canvas.width / 2;
 let y = canvas.height / 2;
-let dx = 2;
-let dy = -2;
+let dx = 4;
+let dy = -4;
 
 let mouseY = canvas.height / 2; // Position de la souris initiale
-let gameRunning = false; // État du jeu
+let gameRunning = false; 
 let winner = ''; // Nom du joueur gagnant
 
 let isAIEnabled = false; // Indique si l'IA est activée
-const aiSpeed = 2; // Vitesse de l'IA (pour ajuster sa difficulté)
+const aiSpeed = 4; // Vitesse de l'IA (pour ajuster sa difficulté)
 
 // Fonction pour dessiner la palette
-function drawPaddle(x, y) {
+function drawPaddle(x, y) 
+{
     context.beginPath();
     context.rect(x, y, paddleWidth, paddleHeight);
     context.fillStyle = '#0095DD';
@@ -28,7 +32,8 @@ function drawPaddle(x, y) {
 }
 
 // Fonction pour dessiner la balle
-function drawBall() {
+function drawBall() 
+{
     context.beginPath();
     context.arc(x, y, ballRadius, 0, Math.PI * 2);
     context.fillStyle = '#0095DD';
@@ -37,7 +42,8 @@ function drawBall() {
 }
 
 // Fonction pour dessiner le message de perte
-function drawLossMessage() {
+function drawLossMessage() 
+{
     context.font = '30px Arial';
     context.fillStyle = 'red';
     context.textAlign = 'center';
@@ -46,29 +52,29 @@ function drawLossMessage() {
 }
 
 // Fonction pour gérer le mouvement de l'IA
-function moveAI() {
-    if (y > paddleY2 + paddleHeight / 2) {
+function moveAI() 
+{
+    if (y > paddleY2 + paddleHeight / 2) 
         paddleY2 += aiSpeed;
-    } else {
+    else
         paddleY2 -= aiSpeed;
-    }
 
     // L'IA perd volontairement parfois (10% de chances)
-    if (Math.random() < 0.1) {
+    if (Math.random() < 0.1) 
         paddleY2 += aiSpeed * 2; // Déplace l'IA loin de la balle
-    }
 
     // Empêche l'IA de sortir du canvas
-    if (paddleY2 < 0) {
+    if (paddleY2 < 0)
         paddleY2 = 0;
-    }
-    if (paddleY2 > canvas.height - paddleHeight) {
+
+    if (paddleY2 > canvas.height - paddleHeight) 
         paddleY2 = canvas.height - paddleHeight;
-    }
+
 }
 
 // Fonction pour dessiner le jeu
-function draw() {
+function draw() 
+{
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawPaddle(0, paddleY1); // Palette du joueur 1
     drawPaddle(canvas.width - paddleWidth, paddleY2); // Palette du joueur 2 ou de l'IA
@@ -78,74 +84,82 @@ function draw() {
     paddleY1 = mouseY - paddleHeight / 2;
 
     // Gère le mouvement de l'IA si elle est activée
-    if (isAIEnabled) {
+    if (isAIEnabled) 
         moveAI();
-    }
 
     // Vérifie si la balle touche le bord gauche ou droit du canvas (conditions de perte)
-    if (x + dx < ballRadius) {
-        winner = 'Player 2';
+    if (x + dx < ballRadius) 
+    {
+        winner = isAIEnabled ? 'AI' : 'Player 2';
         gameRunning = false;
         drawLossMessage();
         return;
-    } else if (x + dx > canvas.width - ballRadius) {
-        winner = isAIEnabled ? 'AI' : 'Player 1';
+    } 
+    else if (x + dx > canvas.width - ballRadius) 
+    {
+        winner = 'Player 1';
         gameRunning = false;
         drawLossMessage();
         return;
     }
 
     // Vérifie les collisions avec les palettes
-    if (x + dx < paddleWidth + ballRadius && y > paddleY1 && y < paddleY1 + paddleHeight) {
+    if (x + dx < paddleWidth + ballRadius && y > paddleY1 && y < paddleY1 + paddleHeight) 
         dx = -dx;
-    } else if (x + dx > canvas.width - paddleWidth - ballRadius && y > paddleY2 && y < paddleY2 + paddleHeight) {
+    else if (x + dx > canvas.width - paddleWidth - ballRadius && y > paddleY2 && y < paddleY2 + paddleHeight)
         dx = -dx;
-    }
+    
 
     // Vérifie les collisions avec les murs (haut et bas)
-    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius)
         dy = -dy;
-    }
 
     x += dx;
     y += dy;
 
-    if (gameRunning) {
+    if (gameRunning) 
         requestAnimationFrame(draw);
-    }
+    
 }
 
-async function isPlayer2Registered() {
-    try {
+//Verifie si joueur 1 est solo
+async function isPlayer2Registered() 
+{
+    try 
+    {
         const response = await fetch(`${apiUrl}/tournament`);
-        if (!response.ok) {
+        if (!response.ok) 
             throw new Error('Network response was not ok');
-        }
+        
         const tournaments = await response.json();
         // Vérifier s'il y a un tournoi en cours avec un joueur 2
-        for (const tournament of tournaments) {
+        for (const tournament of tournaments) 
+        {
             const player2 = tournament.players.find(player => player.alias === 'Player2');
-            if (player2) {
+            if (player2) 
                 return true; // Joueur 2 trouvé
-            }
         }
-        return false; // Joueur 2 non trouvé
-    } catch (error) {
+        return false;
+    } 
+    catch (error) 
+    {
         console.error('Error checking Player 2 registration:', error);
         return false;
     }
 }
 
-function player2Exists() {
+function player2Exists() 
+{
     return isPlayer2Registered().then(isRegistered => {
-        return isRegistered; // Renvoie true si le joueur 2 est enregistré, sinon false
+        return isRegistered; // Renvoie true si le joueur 2 est enregistré
     }).catch(error => {
         console.error('Error checking if Player 2 exists:', error);
-        return false; // En cas d'erreur, supposez que le joueur 2 n'est pas présent
+        return false; // En cas d'erreur, on suppose que le joueur 2 n'est pas présent
     });
 }
 
-function startGame() {
+function startGame() 
+{
     player2Exists().then(exists => {
         isAIEnabled = !exists; // Active l'IA si le joueur 2 n'existe pas
         document.getElementById('pongCanvas').style.display = 'block'; // Affiche le canvas
@@ -153,13 +167,14 @@ function startGame() {
         gameRunning = true; // Démarre le jeu
         x = canvas.width / 2;
         y = canvas.height / 2;
-        dx = 2;
-        dy = -2;
-        draw(); // Démarre le dessin
+        dx = 4;
+        dy = -4;
+        draw(); // Démarre l'affichage
     });
 }
 
-function restartGame() {
+function restartGame() 
+{
     winner = '';
     startGame();
 }
@@ -173,12 +188,11 @@ canvas.addEventListener('mousemove', (event) => {
     mouseY = event.clientY - rect.top; // Ajuste la position Y de la souris relative au canvas
 
     // Empêche la palette de sortir du canvas
-    if (mouseY < paddleHeight / 2) {
+    if (mouseY < paddleHeight / 2) 
         mouseY = paddleHeight / 2;
-    }
-    if (mouseY > canvas.height - paddleHeight / 2) {
+
+    if (mouseY > canvas.height - paddleHeight / 2) 
         mouseY = canvas.height - paddleHeight / 2;
-    }
 });
 
 // Ajoute un gestionnaire d'événements pour le bouton de démarrage
@@ -190,4 +204,3 @@ restartButton.textContent = 'Restart Game';
 document.body.appendChild(restartButton);
 restartButton.style.display = 'none'; // Cache le bouton au départ
 restartButton.addEventListener('click', restartGame);
-
