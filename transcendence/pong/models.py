4f5ff513d7f11e7168ad7b925e97a01141703e66
jleguay <jleguay@student.42.fr>
2024-11-04 13:37:52 +0100

    just moving files to static

--- transcendence/pong/models.py
+++ transcendence/pong/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	username = models.fields.CharField(max_length=100) # gestion d erreur : bloquer la taille lors de la creation du username
+	password = models.fields.CharField(max_length=100) # gestion d erreur : bloquer la taille lors de la creation du password
+	score = models.fields.IntegerField()
+
+class Chat(models.Model):
+	message = models.fields.CharField(max_length=100)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
