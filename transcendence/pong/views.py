5abcd97baf279b3b392fc40a309739d67c1c80a9
jleguay <jleguay@student.42.fr>
2024-11-06 20:58:42 +0100

    -

--- transcendence/pong/views.py
+++ transcendence/pong/views.py
@@ -26,7 +26,7 @@ def index(request):
             print ('-------4---------')
             chat_form = forms.ChatForm(request.POST, prefix='chat')
             if chat_form.is_valid():
-                chat = chat_form.save()
+                chat_form.save()
     return render(request, 'pong/index.html', context={'login_form': login_form, 'chat_form': chat_form, 'message': message})
 
 def logout_user(request):
