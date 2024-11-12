eeb0802d434690345ff308c45bc61fd000769904
jleguay <jleguay@student.42.fr>
2024-11-12 12:28:19 +0100

    htmx

--- transcendence/pong/views.py
+++ transcendence/pong/views.py
@@ -6,33 +6,33 @@ from django.conf import settings
 from pong.models import Chat
 
 def index(request):
-    sign_form = None
-    signin_error_message = ''
+    # sign_form = None
+    # signin_error_message = ''
     chat_form = forms.ChatForm()
     chat_messages = Chat.objects.all()[:10]
-    if request.htmx:
+    if request.method == 'POST':
         print ('>>>>>>POST>>>>>>>', request.POST, '<<<<<<<<<<<<<<')
-        if 'signin' in request.POST:
-            print('ICI')
-            sign_form = forms.LoginForm(prefix="signin")
-            context={'sign_form': sign_form, 'signin_error_message': signin_error_message}
-            return render(request, 'pong/partials/sign.html', context)
-        elif 'signup' in request.POST:
-            sign_form = forms.SignupForm(prefix="signup")
-        elif ('submit' and 'signin-username') in request.POST:
-            sign_form = forms.LoginForm(request.POST, prefix="signin")
-            if sign_form.is_valid():
-                user = authenticate(username = sign_form.cleaned_data['username'], password = sign_form.cleaned_data['password'],)
-                if user is not None:
-                    login(request, user)
-                else:
-                    signin_error_message = 'Wrong credentials'
-        elif ('submit' and 'signup-username') in request.POST:
-            sign_form = forms.SignupForm(request.POST, prefix="signup")
-            if sign_form.is_valid():
-                user = sign_form.save()
-                login(request, user)
-        elif 'logout' in request.POST:
+        # if 'signin' in request.POST:
+        #     print('ICI')
+        #     sign_form = forms.LoginForm(prefix="signin")
+        #     context={'sign_form': sign_form, 'signin_error_message': signin_error_message}
+        #     return render(request, 'pong/partials/sign.html', context)
+        # if 'signup' in request.POST:
+        #     sign_form = forms.SignupForm(prefix="signup")
+        # if ('submit' and 'signin-username') in request.POST:
+        #     sign_form = forms.LoginForm(request.POST, prefix="signin")
+        #     if sign_form.is_valid():
+        #         user = authenticate(username = sign_form.cleaned_data['username'], password = sign_form.cleaned_data['password'],)
+        #         if user is not None:
+        #             login(request, user)
+        #         else:
+        #             signin_error_message = 'Wrong credentials'
+        # elif ('submit' and 'signup-username') in request.POST:
+        #     sign_form = forms.SignupForm(request.POST, prefix="signup")
+        #     if sign_form.is_valid():
+        #         user = sign_form.save()
+        #         login(request, user)
+        if 'logout' in request.POST:
             print('loging out')
             logout(request)
         elif 'chat' in request.POST:
@@ -44,8 +44,24 @@ def index(request):
                 chat_form = forms.ChatForm()
     return render(request,
         'pong/index.html',
-        context={'sign_form': sign_form,
-                 'signin_error_message': signin_error_message,
+        context={'sign_form': None,
+                 'signin_error_message': None,
                  'chat_form': chat_form,
                  'chat_messages': chat_messages,
                  })
+
+def signin(request):
+    sign_form = forms.LoginForm()
+    context = {
+        'sign_form': sign_form,
+        'signin_error_message': None,
+    }
+    return(request, 'pong/partials/sign.html', context)
+
+def signup(request):
+    sign_form = forms.SignupForm()
+    context = {
+        'sign_form': sign_form,
+        'signin_error_message': None,
+    }
+    return(request, 'pong/partials/sign.html', context)
--- transcendence/transcendence/urls.py
+++ transcendence/transcendence/urls.py
@@ -21,4 +21,6 @@ from pong import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('signin/', views.signin, name='signin'),
+	path('signup/', views.signup, name='signup'),
 ]
