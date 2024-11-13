352e6b37d32cc15a1f929fe5163dde56a49c3306
jleguay <jleguay@student.42.fr>
2024-11-13 21:33:59 +0100

    -

--- transcendence/pong/models.py
+++ transcendence/pong/models.py
@@ -13,6 +13,7 @@ class User(AbstractUser):
 class Chat(models.Model):
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	content = models.fields.CharField(max_length=100)
+	created = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return f'{self.author.username} : {self.content}'
