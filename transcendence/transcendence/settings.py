a596ba8245dec246bc44f3fe884e3c80f51ac1fc
jleguay <jleguay@student.42.fr>
2024-11-13 19:08:15 +0100

    -

--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
 ]
 
 #WSGI_APPLICATION = 'transcendence.wsgi.application'
-ASGI_APPLICATION = 'transcendence.asgi.application'
+ASGI_APPLICATION = 'routing.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
