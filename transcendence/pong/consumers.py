06691c96c43b2b953263797965222ed188b44ae1
jleguay <jleguay@student.42.fr>
2024-11-14 10:18:30 +0100

    -

--- transcendence/pong/consumers.py
+++ transcendence/pong/consumers.py
@@ -1,7 +1,4 @@
 from channels.generic.websocket import WebsocketConsumer
-from .models import *
-from django.template.loader import render_to_string
-import json
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
