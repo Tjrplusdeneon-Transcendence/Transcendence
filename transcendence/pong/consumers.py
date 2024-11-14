05449cd6c5f90a377be43ef2f0ef86ea5aab5169
jleguay <jleguay@student.42.fr>
2024-11-14 09:53:30 +0100

    -

--- transcendence/pong/consumers.py
+++ transcendence/pong/consumers.py
@@ -1,17 +1,29 @@
 from channels.generic.websocket import WebsocketConsumer
-
+from .models import *
+from django.template.loader import render_to_string
 import json
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        print(self.scope)
+        self.user = self.scope['user']
         self.accept()
 
     # def disconnect(self, close_code):
     #     pass
 
+    def receive(self, event):
+        print(event)
+        self.send({
+            "type": "websocket.send",
+            "text": event["text"],
+        })    
+
     # def receive(self, text_data):
     #     text_data_json = json.loads(text_data)
-    #     message = text_data_json["message"]
-
-    #     self.send(text_data=json.dumps({"message": message}))
+    #     content = text_data_json["content"]
+    #     message = Chat.objects.create(
+    #         content = content,
+    #         author = self.user,
+    #     )
+    #     html = render_to_string('pong/partials/chat_message.html', context={'message': message})
+    #     self.send(text_data=html)
