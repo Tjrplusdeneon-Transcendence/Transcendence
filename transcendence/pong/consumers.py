8e0624306363e921723cd19bbd90e0db343cfb28
jleguay <jleguay@student.42.fr>
2024-11-14 10:28:20 +0100

    -

--- transcendence/pong/consumers.py
+++ transcendence/pong/consumers.py
@@ -1,9 +1,11 @@
 from channels.generic.websocket import WebsocketConsumer
 from . import models
-from django.template.loader import render_to_string
+# from django.template.loader import render_to_string
 
 import json
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
+        print("USER= ", self.user.username)
         self.accept()
