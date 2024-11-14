b6e1c116a8cbb40b78f3a34176d34c713b3ea88c
jleguay <jleguay@student.42.fr>
2024-11-14 10:43:07 +0100

    -

--- transcendence/pong/consumers.py
+++ transcendence/pong/consumers.py
@@ -10,7 +10,9 @@ class ChatConsumer(WebsocketConsumer):
         
     def receive(self, text_data):
         content = (json.loads(text_data))["content"]
+        print("CONTENT: ", content)
+        print("AUTHOR: ", self.user.username)
         message = Chat.objects.create(
             content = content,
-            author = self.user,
+            author = self.user
         )
