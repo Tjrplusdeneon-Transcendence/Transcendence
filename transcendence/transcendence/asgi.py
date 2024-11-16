e4f543fda0a68e1bb592f5135a93c3ef0b09a904
UrNot42 <ulevallo@student.42.fr>
2024-11-16 16:20:29 +0100

    [EDIT] added security properties to asgi.py

--- transcendence/transcendence/asgi.py
+++ transcendence/transcendence/asgi.py
@@ -6,6 +6,14 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
+"""
+ASGI config for transcendence project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
 
 import os
 
@@ -13,18 +21,19 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
+from pong.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendence.settings')
 
 django_asgi_app = get_asgi_application()
 
 from pong import routing
-from pong.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http":  get_asgi_application(),
-    "websocket":  AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
     ),
 })
-
