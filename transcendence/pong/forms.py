fe3bb09a4872513c2c305d1cb9b8e067dce81563
jleguay <jleguay@student.42.fr>
2024-11-21 22:36:44 +0100

    -

--- transcendence/pong/forms.py
+++ transcendence/pong/forms.py
@@ -20,4 +20,4 @@ class ChatForm(forms.ModelForm):
 		model = Chat
 		fields = ('content',)
 	
-	content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Écrire un message...', 'style': 'width: 100%;'}))
+	content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Écrire un message...', 'style': 'width: 100%; height: 3em; resize: none;'}))
