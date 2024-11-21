abe5a429f016e55769bf02d93c29cb4fc1a09d4f
jleguay <jleguay@student.42.fr>
2024-11-21 20:27:37 +0100

    [add] reset form in chat box after submitting message

--- transcendence/pong/routing.py
+++ transcendence/pong/routing.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-	re_path("ws/chat/$", consumers.ChatConsumer.as_asgi()),
+	path("ws/chat", consumers.ChatConsumer.as_asgi()),
 	re_path("ws/pong/$", consumers.PongConsumer.as_asgi()),
 ]
