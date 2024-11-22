1115cb130d8a6062e6f4c06f5b7cb475723978cf
jleguay <jleguay@student.42.fr>
2024-11-22 12:09:24 +0100

    -

---
+++ transcendence/pong/static/pong/scripts/panel.js
@@ -0,0 +1,42 @@
+document.getElementById('loginButton').addEventListener('click', function(event) 
+{
+    event.preventDefault();
+    document.getElementById('loginPanel').style.display = 'block';
+});
+
+document.getElementById('closeButton').addEventListener('click', function() 
+{
+    document.getElementById('loginPanel').classList.add('slide-out');
+    setTimeout(function() 
+    {
+        document.getElementById('loginPanel').style.display = 'none';
+        document.getElementById('loginPanel').classList.remove('slide-out');
+    }, 300);
+});
+
+document.getElementById('logoutButton').addEventListener('click', function() 
+{
+    isLoggedIn = false;
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('profileInfo').style.display = 'none';
+    document.getElementById('gameStats').style.display = 'none'; 
+});
+
+function openProfileModal(username, targetElement) 
+{
+    const modal = document.getElementById('profileModal');
+    const profileUsername = document.getElementById('profileUsername');
+    
+    profileUsername.textContent = username;
+
+    const rect = targetElement.getBoundingClientRect();
+
+    modal.style.top = `${rect.bottom + window.scrollY + 10}px`;
+    modal.style.left = `${rect.left + window.scrollX}px`;
+    modal.style.display = 'block';
+}
+
+document.getElementById('closeProfileModal').addEventListener('click', function() 
+{
+    document.getElementById('profileModal').style.display = 'none';
+});
