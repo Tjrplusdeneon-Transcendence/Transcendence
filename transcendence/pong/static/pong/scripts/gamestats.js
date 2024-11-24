3f94f06f1b3d3250693403997f9602eb0a7529b0
Jeremie Leguay <jleguay@student.42.fr>
2024-11-24 14:43:53 +0100

    [fix] whole html loaded in panel

--- transcendence/pong/static/pong/scripts/gamestats.js
+++ transcendence/pong/static/pong/scripts/gamestats.js
@@ -1,82 +1,69 @@
+function isUserLogged() {
+    return document.body.getAttribute('data-user-authenticated') === 'True';
+}
+
 function increaseWins() {
-    const csrfToken = document.querySelector('body').getAttribute('hx-headers').match(/"X-CSRFToken": "([^"]+)"/)[1];
-    fetch('/increase_wins/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken  // Use the CSRF token retrieved from hx-headers
-        },
-        body: JSON.stringify({})
-    }).then(response => {
-        if (response.ok) {
-            response.text().then(html => {
-                document.getElementById('panel').innerHTML = html; // Update the entire panel
-            });
-        }
-    });
+    if (isUserLogged())
+    {
+        const csrfToken = document.querySelector('body').getAttribute('hx-headers').match(/"X-CSRFToken": "([^"]+)"/)[1];
+        fetch('/increase_wins/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+            body: JSON.stringify({})
+        }).then(response => {
+            if (response.ok) {
+                response.text().then(html => {
+                    document.getElementById('panel').innerHTML = html; // Update the entire panel
+                });
+            }
+        });
+    }
 }
 
 function increaseLosses() {
-    const csrfToken = document.querySelector('body').getAttribute('hx-headers').match(/"X-CSRFToken": "([^"]+)"/)[1];
-    fetch('/increase_losses/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken  // Use the CSRF token retrieved from hx-headers
-        },
-        body: JSON.stringify({})
-    }).then(response => {
-        if (response.ok) {
-            response.text().then(html => {
-                document.getElementById('panel').innerHTML = html; // Update the entire panel
-            });
-        }
-    });
+    if (isUserLogged())
+    {
+        const csrfToken = document.querySelector('body').getAttribute('hx-headers').match(/"X-CSRFToken": "([^"]+)"/)[1];
+        fetch('/increase_losses/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+            body: JSON.stringify({})
+        }).then(response => {
+            if (response.ok) {
+                response.text().then(html => {
+                    document.getElementById('panel').innerHTML = html; // Update the entire panel
+                });
+            }
+        });
+    }
 }
 
 function increaseGamesPlayed() {
-    const csrfToken = document.querySelector('body').getAttribute('hx-headers').match(/"X-CSRFToken": "([^"]+)"/)[1];
-    fetch('/increase_games_played/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken  // Use the CSRF token retrieved from hx-headers
-        },
-        body: JSON.stringify({})
-    }).then(response => {
-        if (response.ok) {
-            response.text().then(html => {
-                document.getElementById('panel').innerHTML = html; // Update the entire panel
-            });
-        }
-    });
+    if (isUserLogged())
+    {
+        console.log("user is logged");
+        const csrfToken = document.querySelector('body').getAttribute('hx-headers').match(/"X-CSRFToken": "([^"]+)"/)[1];
+        fetch('/increase_games_played/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+            body: JSON.stringify({})
+        }).then(response => {
+            if (response.ok) {
+                response.text().then(html => {
+                    document.getElementById('panel').innerHTML = html; // Update the entire panel
+                });
+            }
+        });
+    } else {
+        console.log("user is NOT logged");
+    }
 }
-
-
-// let gamesPlayed = 0;
-// let wins = 0;
-// let losses = 0;
-// let totalScore = 0;
-
-// function updateGameStats(score) 
-// {
-//     gamesPlayed++;
-    
-//     if (score > 0)
-//         wins++;
-//     else
-//         losses++;
-
-//     if (document.getElementById('gamesPlayed')) 
-//     {
-//         totalScore += score;
-        
-//         document.getElementById('gamesPlayed').innerText = gamesPlayed;
-//         document.getElementById('wins').innerText = wins;
-//         document.getElementById('losses').innerText = losses;
-//         document.getElementById('totalScore').innerText = totalScore;
-//     }
-// }
-
-// window.updateGameStats = updateGameStats;
-
