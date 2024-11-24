889f82b03b7df553150190643593b25cff2f39d0
nagvaill <hyperdestroyer8089@gmail.com>
2024-11-24 20:39:59 +0100

    [ADD] SQL protection

--- transcendence/pong/forms.py
+++ transcendence/pong/forms.py
@@ -1,11 +1,24 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
 from pong.models import Chat
 
 class SigninForm(forms.Form):
-	username = forms.CharField(max_length=63, label='Username')
-	password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Password')
+    username = forms.CharField(max_length=63, label='Username')
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Password')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is None:
+                raise forms.ValidationError("Invalid username or password.")
+        return cleaned_data
 
 class SignupForm(UserCreationForm):
 	class Meta(UserCreationForm):
