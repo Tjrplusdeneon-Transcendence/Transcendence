d2dff31a590c208aa062c8a92db41ad696b0462a
jleguay <jleguay@student.42.fr>
2024-11-26 10:20:58 +0100

    Back to DEBUG = True

--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xfo+y4_@e5c-rl@nzr3!ht4h4ocvg7s9(@1l*!!32jad830*%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
