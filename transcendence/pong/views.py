907dd0b27dc8aeb115cd8d171ad6039ae5824bdf
jleguay <jleguay@student.42.fr>
2024-11-26 10:30:35 +0100

    trying to solve issue with csrf token

--- transcendence/pong/views.py
+++ transcendence/pong/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.conf import settings
 from pong.models import Chat
 from django.template.loader import render_to_string
+from django.middleware.csrf import get_token
 
 def view_404(request, exception=None):
     return redirect('index')
@@ -25,14 +26,15 @@ def signin_user(request):
                 login(request, user)
                 chat_messages = Chat.objects.all()[:20]
                 panel_html = render_to_string('pong/partials/panel.html', request=request)
-                chat_html = render_to_string('pong/chat.html', {'chat_messages': chat_messages}, request=request)
+                chat_html = render_to_string('pong/chat.html', context={'chat_messages': chat_messages}, request=request)
                 return JsonResponse({
                     'panel_html': panel_html,
                     'chat_html': chat_html
                 })
             else:
                 signin_error_message = 'Wrong credentials'
-    sign_html = render_to_string('pong/partials/signin.html', context={'sign_form': sign_form, 'signin_error_message': signin_error_message})
+    csrf_token = get_token(request)
+    sign_html = render_to_string('pong/partials/signin.html', context={'csrf_token': csrf_token, 'sign_form': sign_form, 'signin_error_message': signin_error_message})
     panel_html = render_to_string('pong/partials/panel.html', context={'sign_html': sign_html}, request=request)
     return JsonResponse({'panel_html': panel_html})
 
@@ -51,7 +53,8 @@ def signup_user(request):
                 'panel_html': panel_html,
                 'chat_html': chat_html
             })
-    sign_html = render_to_string('pong/partials/signup.html', context={'sign_form': sign_form})
+    csrf_token = get_token(request)
+    sign_html = render_to_string('pong/partials/signup.html', context={'csrf_token': csrf_token, 'sign_form': sign_form})
     panel_html = render_to_string('pong/partials/panel.html', context={'sign_html': sign_html}, request=request)
     return JsonResponse({'panel_html': panel_html})
 
